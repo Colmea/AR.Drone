8a66c4423d636041df1d4002cc3bb741e90c67c0
Arthur Schwaiger <arthur.schwaiger@gmail.com>
2013-07-30 23:04:25 +0200

    Project basis for AR Drone Kinect control
    + WPF project creation
    + WPF design from windows developer toolkit examples
    + Speech Synth

---
+++ AR.Drone.Kinect/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Ar drone - Kinect")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: CLSCompliant(true)]
